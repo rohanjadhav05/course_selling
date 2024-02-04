90424289f3eef3eda612914db9246f2acb48f950
rohanjadhav05 <rohan.jadhav511@gmail.com>
2024-02-04 12:53:23 +0530

    folder structure of course_selling app

---
+++ springboot3security/src/main/java/com/ey/springboot3security/CourseSellingApplication.java
@@ -0,0 +1,13 @@
+package com.ey.springboot3security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseSellingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseSellingApplication.class, args);
+	}
+
+}
